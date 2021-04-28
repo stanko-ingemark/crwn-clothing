4fd2f0adece6280e382e151dc238b283197b7e2b
Stanko Arbutina <stanko.arbutina@gmail.com>
2021-04-28 12:03:26 +0200

    check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import { Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+import Header from './components/header/header.component';
+import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInAndSignUpPage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import './sign-in-and-sign-up.styles.scss';
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+      SIGN IN!
+      <SignIn/>
+    </div>
+)
+
+export default SignInAndSignUpPage;
