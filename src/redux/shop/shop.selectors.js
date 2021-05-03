ebd848cc39f2d7a99119c3a913413c4c8e6146b8
Stanko Arbutina <stanko.arbutina@gmail.com>
2021-05-03 16:31:47 +0200

    Categories

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,16 @@
 import {createSelector} from "reselect";
 
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womans: 4,
+  mans: 5
+}
+
 const shopSelector = state => state.shop;
 
-export const selectCollections = createSelector(shopSelector, shop => shop.collections);
+export const selectCollections = createSelector([shopSelector], shop => shop.collections);
+export const selectCollection = collectionUrlParam => createSelector([selectCollections],
+        collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]))
